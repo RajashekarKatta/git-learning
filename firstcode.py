a61988d64025c515a488c70eb2267adc77b04a12
Katta Rajashekar <rajashekarkatta18@gmail.com>
2025-04-24 17:15:02 +0530

    Added one program to firstcode.py

--- firstcode.py
+++ firstcode.py
@@ -3,3 +3,12 @@ print("Hello World!..")
 print("Katta Rajashekar")
 # the above statement prints "Hello World"
 # thank you eveything
+def something(n):
+    if n == 0:
+        return 0
+    if n %9 == 0:
+        return 9
+    else:
+        return n % 9
+n = 1234
+print(something(n))
